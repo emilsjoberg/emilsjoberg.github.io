4d3b560f0447138f410728d0f9af4ce0dc9a5cf8
emilsjoberg <js3259@cornell.edu>
2016-07-21 14:01:44 -0400

    Add files via upload

---
+++ background/vis.js
@@ -0,0 +1,40 @@
+ /**
+
+Resume concept based on the Dribbble by John Wilson http://dribbble.com/shots/900308-Resume?list=users
+
+**/
+
+$(document).ready(function() {
+  $('#item1').animate({
+     borderBottomWidth: '50px'}, 1000);
+  $('#item2').animate({
+     borderBottomWidth: '50px'}, 1000);
+  $('#item3').animate({
+     borderBottomWidth: '50px'}, 1000);
+  $('#item4').animate({
+     borderBottomWidth: '50px'}, 1000);
+  $('#item5').animate({
+     borderBottomWidth: '50px'}, 1000);
+  $('#item6').animate({
+     borderBottomWidth: '150px'}, 1000);
+  $('#item7').animate({
+     borderBottomWidth: '150px'}, 1000);
+  $('#item8').animate({
+     borderBottomWidth: '150px'}, 1000);
+  $('#item9').animate({
+     borderBottomWidth: '150px'}, 1000);
+  $('#item10').animate({
+     borderBottomWidth: '150px'}, 1000);
+  $('#item11').animate({
+     borderBottomWidth: '100px'}, 1000);
+  $('#item12').animate({
+     borderBottomWidth: '150px'}, 1000);
+
+})
+
+$(".html").animate({width:"90%"},2000);
+$(".css").animate({width:"90%"},2000);
+$(".php").animate({width:"70%"},2000);
+$(".coldf").animate({width:"80%"},2000);
+$(".graphic").animate({width:"85%"},2000);
+$(".jquery").animate({width:"60%"},2000);
